f75844e666401a3310227bfd7ecae9e8d8092c9d
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-05-18 13:42:57 +0300

    siparis islemleri icin kullanici_detay migration dosyası olusturuldu

---
+++ database/migrations/2023_05_18_103547_create_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('adress',200);
+            $table->string('phone',11);
+            $table->string('mobile_phone',11);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
