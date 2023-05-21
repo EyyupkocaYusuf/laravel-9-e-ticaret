beb462d12dfba531c26fdeafeefd43fc59f79838
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-05-21 15:24:02 +0300

    footerde düzeltme yapıldı

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,6 @@
         <h1>404</h1>
         <h2>Aradıgınız Sayfa Bulunamadı !</h2>
         <a href="{{route('home')}}" class="btn btn-primary">Ana Sayfaya Dön</a>
-        <h5>{{$exception}}</h5>
     </div>
 </div>
 @endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,8 +1,7 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-6">Eticaret Yazılımı &copy; 2017</div>
-            <div class="col-md-6 text-right"><a href="http://www.uzaktankurs.com"></a></div>
+
         </div>
     </div>
 </footer>
